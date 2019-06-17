30c618bb7a3f589182586315dd63b1290fde98ec
krill <lpmaze@gmail.com>
2019-06-17 08:49:27 -0400

    added js for button & did css work for form

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let form = document.getElementById("add-book-btn");
+
+// ################------ Books ------################
 let library = [];
 
 function Book(title, author, pages, read){
@@ -14,14 +17,6 @@ function addBookToLibrary(book) {
     library.push(book);
 }
 
-const book1 = new Book('Lord of the Rings', 'J.R.R. Tolkien', 456, true);
-const book2 = new Book('Atlas Shrugged', 'Ayn Rand', 675, false);
-const book3 = new Book('Teen Spirit', 'Kurt Cobain', 1, true);
-
-addBookToLibrary(book1);
-addBookToLibrary(book2);
-addBookToLibrary(book3);
-
 function render(){
     let bookUl = '';
     for(let book in library){
@@ -31,6 +26,55 @@ function render(){
     document.getElementById("book-list").innerHTML = bookUl;
 }
 
+// ################------ Display add books/form ------################
+// shows the form to add a book and hide add book button
+function addBookForm(){
+    let form = `
+    <form id="add-book-form">
+    <div>
+        <label>Title:</label>
+        <input type="text" name="title" id="title" required />
+    </div>
+    <div>
+        <label>Author:</label>
+        <input type="text" name="author" id="author" required />
+    </div>
+    <div>
+        <label>Pages:</label>
+        <input type="text" name="pages" id="pages" required />
+    </div>
+    <div>
+        <label>Read:</label>
+        <input type="checkbox" name="read" id="read" />
+    </div>
+    <input type="submit" id="add-book-btn" value="add"> <button onclick="hideForm()">nvm</button>
+</form>`
+
+    //document.getElementById("controls").style.visibility = "hidden";
+    document.getElementById("controls").innerHTML = form;
+}
+
+function hideForm(){
+    let controls = '<button onclick="addBookForm()">Add Book</button>';
+
+    document.getElementById("controls").innerHTML = controls;
+}
+
+
+// ################------ add default books ------################
+const book1 = new Book('Lord of the Rings', 'J.R.R. Tolkien', 456, true);
+const book2 = new Book('Atlas Shrugged', 'Ayn Rand', 675, false);
+const book3 = new Book('Teen Spirit', 'Kurt Cobain', 1, true);
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
+
+// ##### Run functions #####
+
+//form.onsubmit = addBookToLibrary(new Book(form.name.value));
+//form.onsubmit = console.log(form.title.value);
+console.log(render());
 render();
 
 
